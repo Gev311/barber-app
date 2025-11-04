8d099216690b98cc7b70cd59027ecaa8de0ec50b
Gevorg <g.ajabkhanyan@gmail.com>
2025-11-04 18:04:54 +0400

    First

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { ServiceList } from './pages/services/serviceList'
+import './App.css'
+
+function App() {
+
+  return (
+    <ServiceList />
+  )
+}
+
+export default App
---
+++ src/pages/services/ServiceList.jsx
@@ -0,0 +1,23 @@
+import './serviceList.css'
+import barbers from '../../data/barbers.json';
+export const ServiceList = () => {
+    return (
+        <section id="main-layout">
+            <div id="navbar"></div>
+
+            <div id="content"></div>
+            <div className="serviceCardWrapper">
+                {barbers.map((barber) => (
+                    <div className="servicecard" key={barber.id}>
+                        <div className="servicecard-content">
+                            <h2>{barber.name}</h2>
+                            <p>{barber.service}</p>
+                            <p className="price">Price: {barber.price} AMD</p>
+                            <button>Book Now</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+};
